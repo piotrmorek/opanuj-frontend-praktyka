d99356196b491e00fc12842b407b0a3ac7c686ac
Piotr Morek <p.morek@smartrecruiters.com>
2024-07-02 14:05:26 +0200

    feat: simplify the logic

--- examples/module1/lesson4/abort-feedback/App.tsx
+++ examples/module1/lesson4/abort-feedback/App.tsx
@@ -7,18 +7,6 @@ interface User {
 
 const API_URL = '/api/data/users?timeout=10000';
 
-const getNotificationAboutTimeout = (
-  message: string,
-  timeoutInMs: number,
-): Promise<string> => {
-  let timeout: NodeJS.Timeout;
-  return new Promise<string>((resolve) => {
-    timeout = setTimeout(() => resolve(message), timeoutInMs);
-  }).finally(() => clearTimeout(timeout));
-};
-
-const CONNECTIVITY_PROBLEM_MESSAGE = 'CONNECTIVITY_PROBLEM_MESSAGE';
-
 const App = () => {
   const [users, setUsers] = useState<User[]>([]);
   const [connectivityProblem, setConnectivityProblem] = useState(false);
@@ -31,18 +19,17 @@ const App = () => {
     }
     abortController = new AbortController();
 
-    const connectivityProblem: string | void = await Promise.race([
-      fetch(API_URL, { signal: abortController.signal })
-        .then((res) => res.json())
-        .then(({ users }) => {
-          setUsers(users);
-          setConnectivityProblem(false);
-        }),
-      getNotificationAboutTimeout(CONNECTIVITY_PROBLEM_MESSAGE, 5000),
-    ]);
-    if (connectivityProblem === CONNECTIVITY_PROBLEM_MESSAGE) {
+    const timeout = setTimeout(() => {
       setConnectivityProblem(true);
-    }
+    }, 5000);
+
+    fetch(API_URL, { signal: abortController.signal })
+      .then((res) => res.json())
+      .then(({ users }) => {
+        setUsers(users);
+        setConnectivityProblem(false);
+      })
+      .finally(() => clearTimeout(timeout));
   };
 
   useEffect(() => {
